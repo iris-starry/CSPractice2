2760f9f461bf5c01abdde150d5e13c868f2b1401
iris-starry <iris_starry_@naver.com>
2024-07-03 09:03:04 +0900

    #6 23-7. 새도잉

--- CSPractice2/Program.cs
+++ CSPractice2/Program.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace CSPractice2
+namespace CSPractice312
 {
     internal class Program
     {
+        int number = 10;
         static void Main(string[] args)
         {
+            // #6 23-7. 새도잉
+            int number = 20;
+            Console.WriteLine(number); // 20
         }
     }
 }
